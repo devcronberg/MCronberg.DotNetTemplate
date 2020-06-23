69d42cffb69e9db848ace778b76157a99dea5aa6
admin <admin@TA-821-INS>
2020-06-23 12:39:07 +0200

    start

---
+++ console-async/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace console_async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Start");
+            await Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(1000);
+            });            
+            System.Console.WriteLine("End");
+        }
+    }
+}
---
+++ install.bat
@@ -0,0 +1,7 @@
+cd aspnet-mvc-start
+dotnet new -i ./
+cd..
+cd console-async
+dotnet new -i ./
+cd..
+pause
