6aad9c4a8595dd5d2125d88291e67f7b981cb5f6
Michell Cronberg <michell@cronberg.dk>
2020-06-25 00:33:19 +0200

    .

--- run_console_async.bat
+++ run_console_async.bat
@@ -1,7 +1,7 @@
 @echo off
 if not exist "C:\kursus\" mkdir C:\kursus
 c:
-cd kursus
+cd\kursus
 set /p navn="App navn: "
 dotnet new console_async -n %navn%
 cd %navn%
--- run_mvcstart.bat
+++ run_mvcstart.bat
@@ -1,7 +1,7 @@
 @echo off
 if not exist "C:\kursus\" mkdir C:\kursus
 c:
-cd kursus
+cd\kursus
 set /p navn="App navn: "
 dotnet new mvcstart -n %navn%
 cd %navn%
